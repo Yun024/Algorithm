e7d45e382252139ef8959a72ad9991711e35dc85
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2022-10-25 16:10:46 +0900

    Delete Level 0 directory

--- Level 0/세균 증식/세균 증식.py
+++
@@ -1,7 +0,0 @@
-def solution(n, t):
-    answer = n
-    i = 1 
-    while i<=t:
-        answer = answer * 2
-        i = i +1 
-    return answer
--- Level 0/숨어있는 숫자의 덧셈/숨어있는 숫자의 덧셈 （1）.py
+++
@@ -1,3 +0,0 @@
-import re
-def solution(my_string):
-    return sum(list(map(int,re.findall("\d",my_string))))
