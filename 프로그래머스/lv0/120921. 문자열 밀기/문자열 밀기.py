917418bb1819358d0c1f7912282813a4d6d5a5c5
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2022-12-13 09:48:34 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,9 @@
+def solution(A, B):
+    answer =0
+    while A!= B:
+        A= A[-1]+ A[:len(A)-1]
+        answer +=1 
+        if answer == len(A):
+            answer = -1
+            break
+    return answer
