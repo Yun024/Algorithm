b49b23fbd6ef845f51610ef0f973c30dada553fa
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-11 16:46:03 +0900

    Delete 프로그래머스/lv0/120907. OX퀴즈 directory

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
+++
@@ -1,8 +0,0 @@
-def solution(quiz):
-    answer =[]
-    for i in [i.split("=") for i in quiz]:
-        if eval(i[0]) == int(i[1]):
-            answer.append("O")
-        else:
-            answer.append("X")
-    return answer
