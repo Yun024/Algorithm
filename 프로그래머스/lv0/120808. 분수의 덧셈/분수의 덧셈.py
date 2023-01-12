6a3f825a6b8b4167e231a5ff890305d1f0d59f54
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-12 11:05:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.94 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,28 @@
+def multp(array):
+    answer= 1
+    for i in array:
+        answer *= i
+    return answer
+def solution(numer1, denom1, numer2, denom2):
+    denom = denom1 * denom2
+    final = [[],[]]
+    answer = [numer1 * denom/denom1 + numer2 * denom/denom2, denom ]
+
+    for j,k in enumerate(answer):
+        i =1
+        while i != k:
+            if k/i == int(k/i):
+                final[j].append(i)
+                k = k/i
+                i = 2
+            else :
+                i +=1
+        final[j].append(i)
+    for i in final[0]:
+        if i in final[1]:
+            final[1].remove(i)
+    if multp(final[1]) == answer[1]:
+        return answer
+    else :
+        ans = answer[1]/multp(final[1])
+        return list(map(lambda x: x/ans,answer))
