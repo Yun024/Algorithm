4f3d0221888ce0a856e63a7dc74385e015833db5
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-19 15:56:49 +0900

    Delete 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자 directory

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++
@@ -1,8 +0,0 @@
-def solution(x, n):
-    if x>0:
-        return [i for i in range(x,x*n+1,x)]
-    elif x==0:
-        return [0]*n
-    else:
-        x = abs(x)
-        return [i*-1 for i in range(x,x*n+1,x)]
