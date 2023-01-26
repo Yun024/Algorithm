f1c78dc6701dfebc155091925f0f67fee2aa1298
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-26 14:11:35 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,6 +1,7 @@
 def solution(s):
-    numlist = [str(i) for i in range(10)]
-    cond1 = len(s) == 4
-    cond2 = len(s) == 6
-    cond3 = ''.join([i for i in s if i in numlist]) == s
-    return (cond1 or cond2) and cond3
+    # numlist = [str(i) for i in range(10)]
+    # cond1 = len(s) == 4
+    # cond2 = len(s) == 6
+    # cond3 = ''.join([i for i in s if i in numlist]) == s
+    return s.isdigit() and len(s) in [4,6]
+ 
