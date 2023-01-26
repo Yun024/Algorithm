daaab37bd19e077ec6b9084b4e702db4adea5b41
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-26 16:54:21 +0900

    [level 1] Title: 행렬의 덧셈, Time: 33.93 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,9 +1,16 @@
 
-def solution(arr1, arr2):
-    final =[] 
-    answer = [i+j for i,j in zip(sum(arr1,[]),sum(arr2,[]))]
-    key = len(answer)/len(arr1)
-    for i in range(len(arr1)):
-        final.append(answer[:int(key)])
-        answer = answer[int(key):]
-    return final
+# def solution(arr1, arr2):
+#     final =[] 
+#     answer = [i+j for i,j in zip(sum(arr1,[]),sum(arr2,[]))]
+#     key = len(answer)/len(arr1)
+#     for i in range(len(arr1)):
+#         final.append(answer[:int(key)])
+#         answer = answer[int(key):]
+#     return final
+
+# def solution(arr1,arr2):
+#     return [[a+b for a,b in zip(A,B)] for A,B in zip(arr1,arr2)]
+
+import numpy as np
+def solution(arr1,arr2):
+    return (np.array(arr1) + np.array(arr2)).tolist()
