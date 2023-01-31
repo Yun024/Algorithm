27b795e7898378639415877e28e6030f59fc3d09
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-31 10:50:58 +0900

    Update 이상한 문자 만들기.py

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,12 +1,13 @@
+# def solution(s):
+#     final =[]
+#     for i in s.split(" "):
+#         answer = ""
+#         for j,e in enumerate(i):
+#             if j%2 == 0:
+#                 answer += e.upper()
+#             else:
+#                 answer += e.lower()
+#         final.append(answer)
+#     return ' '.join(final)
 def solution(s):
-    final =[]
-    for i in s.split(" "):
-        answer = ""
-        for j,e in enumerate(i):
-            if j%2 == 0:
-                answer += e.upper()
-            else:
-                answer += e.lower()
-        final.append(answer)
-    return ' '.join(final)
-    
+    return ' '.join([''.join([e.upper() if j%2 ==0 else e.lower() for j,e in enumerate(i)]) for i in s.split(" ")])
