a21c45a94990606a09ae415921a8fe96c8f79b3c
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-31 16:11:04 +0900

    Update ［1차］ 비밀지도.py

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,12 +1,23 @@
-
+# 첫번째 풀이
 def solution(n, arr1, arr2):
-    arr1 = [bin(i).replace("b","")[-n:] for i in arr1]
-    arr2 = [bin(i).replace("b","")[-n:] for i in arr2]
+    arr1 = ''.join([bin(i).replace("b","")[-n:].zfill(n) for i in arr1])
+    arr2 = ''.join([bin(i).replace("b","")[-n:].zfill(n) for i in arr2])
     
-    arr1 = ''.join([i if len(i)==n else "0"*(n-len(i))+i for i in arr1])
-    arr2 = ''.join([i if len(i)==n else "0"*(n-len(i))+i for i in arr2])
+    ### zfill로 대체 가능
+    # arr1 = ''.join([i if len(i)==n else "0"*(n-len(i))+i for i in arr1])
+    # arr2 = ''.join([i if len(i)==n else "0"*(n-len(i))+i for i in arr2])
     
     answer = ''.join(["0" if j=="0"and i=="0" else "1" for j,i in zip(arr1,arr2)]).replace("1","#").replace("0"," ")
     
-    
     return [answer[i:i+n] for i in range(0,len(answer),n)]
+
+# 두번째 풀이
+def solution(n,arr1,arr2):
+    answer =[]
+    for j,i in zip(arr1,arr2):
+        answer.append(bin(j|i)[2:].zfill(n).replace("1","#").replace("0"," "))
+    return answer
+
+# 세번째 풀이        
+def solution(n, arr1,arr2):
+    return [bin(j|i)[2:].zfill(n).replace("1","#").replace("0"," ") for j,i in zip(arr1,arr2)]
