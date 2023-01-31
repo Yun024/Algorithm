2bfcaa1b5eda75a59cef1c6f0377d54e71fe3478
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-31 11:12:38 +0900

    [level 1] Title: 예산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,9 +1,16 @@
+# def solution(d, budget):
+#     for j,i in enumerate(sorted(d)):
+#         budget -= i
+#         if budget < 0:
+#             return j
+#         elif budget == 0:
+#             return j+1
+#     return len(d)
+
+
 def solution(d, budget):
-    for j,i in enumerate(sorted(d)):
-        budget -= i
-        if budget < 0:
-            return j
-        elif budget == 0:
-            return j+1
+    d.sort()
+    while budget < sum(d):
+        d.pop()
     return len(d)
     
