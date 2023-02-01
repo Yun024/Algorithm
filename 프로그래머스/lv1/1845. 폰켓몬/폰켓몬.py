bfe9b22a15240977f191cc466e8fd4aa62132c44
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-02-01 11:30:49 +0900

    Update 폰켓몬.py

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,9 @@
-import itertools 
 def solution(nums):
     get = len(nums)/2
     unique = len(set(nums))
     if get > unique:
         return unique
     return get
+
+def solution(nums):
+    return len(set(nums)) if len(nums)/2 > len(set(nums)) else len(nums)/2
