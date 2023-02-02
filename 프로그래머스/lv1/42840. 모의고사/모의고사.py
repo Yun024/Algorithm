1c321caee67ff8294f15849bb4a722ead4638be0
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-02-02 13:46:01 +0900

    [level 1] Title: 모의고사, Time: 3.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -3,6 +3,12 @@ def solution(answers):
     a = [1,2,3,4,5]
     b = [2,1,2,3,2,4,2,5]
     c = [3,3,1,1,2,2,4,4,5,5]
-    answer = [[a[i%len(a)]==j,b[i%len(b)]==j,c[i%len(c)]==j] for i,j in enumerate(answers)]
-    final = sum(sum(answer,[])[::3]),sum(sum(answer,[])[1::3]),sum(sum(answer,[])[2::3])
-    return [j+1 for j,i in enumerate(final) if i==max(final)]
+    a_ans,b_ans,c_ans = 0,0,0
+    for i,j in enumerate(answers):
+        a_ans+=(a[i%len(a)]==j)
+        b_ans+=(b[i%len(b)]==j)
+        c_ans+=(c[i%len(c)]==j)
+    answer = [a_ans,b_ans,c_ans]
+    #answer = [[a[i%len(a)]==j, b[i%len(b)]==j , c[i%len(c)]==j] for i,j in enumerate(answers)]
+    #final = sum(sum(answer,[])[::3]),sum(sum(answer,[])[1::3]),sum(sum(answer,[])[2::3])
+    return [j+1 for j,i in enumerate(answer) if i==max(answer)]
