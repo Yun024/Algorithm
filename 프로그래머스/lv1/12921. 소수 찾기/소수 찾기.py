aabab3a10f652abbf053b44e5e6eaa0100207856
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-02-02 09:40:29 +0900

    [level 1] Title: 소수 찾기, Time: 252.50 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,49 +1,8 @@
-# def solution(n):
-#     answer = [2]
-#     for i in range(3,n+1,2):
-#         if len([j for j in answer if i%j==0])==0:
-#             answer.append(i)
-#     return len(answer)  
-
-    
-# def solution(n):
-#     answer =[2]
-#     for i in range(3,n+1,2):
-#         ans = 0 
-#         for j in answer:
-#             if ans!=0 or j>(i+1)**0.5:
-#                 break
-#             elif i%j ==0:
-#                 ans+=1
-#         if ans==0:
-#             answer.append(i)
-#     return len(answer)
-
 def solution(n):
-    num = set(list(range(2,n+1)))
-    for i in range(2,n+1):
+    num = set(range(2,n+1))
+    
+    for i in range(2,int(n**0.5)+1):
         if i in num:
             num -= set(range(2*i,n+1,i))
     return len(num)
         
-
-
-# def solution(n):
-#     answer = []
-#     for i in range(2,n+1):
-#         ans = []
-#         for j in range(2,i):
-#             if len(ans)>0 or j>(i+1)//2:
-#                 break
-#             elif i%j==0:
-#                 ans.append(j)
-#         if len(ans)==0:
-#             answer.append(i)
-#     return len(answer)
-            
-# def solution(n):
-#     answer = []
-#     for i in range(2,n+1):
-#         if len([j for j in range(2,i) if i%j ==0] )==0:
-#             answer.append(i)
-#     return answer
