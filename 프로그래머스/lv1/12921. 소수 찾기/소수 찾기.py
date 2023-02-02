ba9d03eb62cad8ba02ae4586a8754c1a07707209
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-02-02 09:25:25 +0900

    [level 1] Title: 소수 찾기, Time: 343.91 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -6,20 +6,27 @@
 #     return len(answer)  
 
     
-def solution(n):
-    answer =[2]
-    for i in range(3,n+1,2):
-        ans = 0 
-        for j in answer:
-            if ans!=0 or j>(i+1)**0.5:
-                break
-            elif i%j ==0:
-                ans+=1
-        if ans==0:
-            answer.append(i)
-    return len(answer)
+# def solution(n):
+#     answer =[2]
+#     for i in range(3,n+1,2):
+#         ans = 0 
+#         for j in answer:
+#             if ans!=0 or j>(i+1)**0.5:
+#                 break
+#             elif i%j ==0:
+#                 ans+=1
+#         if ans==0:
+#             answer.append(i)
+#     return len(answer)
 
+def solution(n):
+    num = set(list(range(2,n+1)))
+    for i in range(2,n+1):
+        if i in num:
+            num -= set(range(2*i,n+1,i))
+    return len(num)
         
+
 
 # def solution(n):
 #     answer = []
