ea68ef71fab5ecc2c779a9525f7f7676c9df220b
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-03-28 17:50:42 +0900

    Update 즐겨찾기가 가장 많은 식당 정보 출력하기.sql

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
+
+## where절 서브쿼리(중첩서브쿼리) 사용 * in을 이용한 다중열 
 select food_type,rest_id,rest_name,favorites
 from rest_info
 where (food_type,favorites) in
@@ -9,4 +11,14 @@ where (food_type,favorites) in
 )
 order by food_type desc
 
+## over함수, partition by, rank 함수 사용 
+select food_type, rest_id, rest_name, favorites
+from 
+(
+    select food_type, rest_id,rest_name, favorites,
+    rank() over(partition by food_type order by favorites desc) as rk
+    from REST_INFO
+)
+where rk = 1
+order by food_type desc
 
