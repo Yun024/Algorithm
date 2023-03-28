20b7c5ab079e4895e161f565b8fd1c43844ddffe
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-03-28 15:01:13 +0900

    Update 카테고리 별 도서 판매량 집계하기.sql

--- 프로그래머스/unrated/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/unrated/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
+#### 안시조인, 서브쿼리 사용, 선두컬럼에 to_char을 사용하여 비교 
 select category, sum(sales) total_sales
 from 
 (
@@ -11,6 +11,11 @@ group by category
 order by category
 
 
--- select * 
--- from book A , book_sales B
--- where A.book_id = B.book_id
+#### 서브쿼리 사용x, between사용, 오라클 조인(join대신 where) 사용 
+
+select A.category, sum(B.sales) total_sales
+from book A , book_sales B
+where A.book_id = B.book_id and
+B.sales_date between to_date('2022-01-01','YYYY-MM-DD') and to_date('2022-01-31','YYYY-MM-DD') 
+group by A.category
+order by A.category
