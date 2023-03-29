2b21c99071697adb42317f112a05b0a2e5a7eb98
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-03-29 10:39:55 +0900

    Update 대여 기록이 존재하는 자동차 리스트 구하기.sql

--- 프로그래머스/unrated/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ 프로그래머스/unrated/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -1,8 +1,17 @@
 -- 코드를 입력하세요
+## group by를 사용한 중복 제거 
 SELECT C.car_id
 from car_rental_company_car C inner join car_rental_company_rental_history R
 on C.car_id = R.car_id
 where car_type = '세단'
 and to_char(start_date,'YYYY-MM') = '2022-10'
 group by C.car_id
+order by car_id desc
+
+## distinct를 사용한 중복 제거 
+SELECT distinct C.car_id
+from car_rental_company_car C inner join car_rental_company_rental_history R
+on C.car_id = R.car_id
+where car_type = '세단'
+and to_char(start_date,'YYYY-MM') = '2022-10'
 order by car_id desc
