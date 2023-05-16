71f2fac2299510e7f535c4a6137de62a2f3c638b
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-05-16 16:38:33 +0900

    Update 접두사인지 확인하기.py

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,2 +1,5 @@
 def solution(my_string, is_prefix):
-    return int(is_prefix in [my_string[:i+1] for i in range(len(my_string))] )
+    return int(is_prefix == my_string[:len(is_prefix)])
+    
+    #return int(my_string.startswith(is_prefix))
+    #return int(is_prefix in [my_string[:i+1] for i in range(len(my_string))] )
