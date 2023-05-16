f9eec3ae712f656cdc09947b02f94aeb817ee0fd
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-05-16 13:49:50 +0900

    Update 길이에 따른 연산.py

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,6 +1,9 @@
 from functools import reduce
-
 def solution(num_list):
+        return   reduce(lambda x,y: x*y , num_list) if len(num_list) <= 10 else sum(num_list)
+    
         
-    return   reduce(lambda x,y: x*y , num_list) if len(num_list) <= 10 else sum(num_list)
+import math
+def solution(num_list):
+        return   math.prod(num_list) if len(num_list) <= 10 else sum(num_list)
     
