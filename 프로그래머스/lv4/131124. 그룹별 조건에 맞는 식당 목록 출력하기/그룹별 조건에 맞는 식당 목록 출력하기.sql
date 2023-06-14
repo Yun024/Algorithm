582f13e850c329db977adac3a0fe3f337dbebb50
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-06-14 23:31:37 +0900

    Update 그룹별 조건에 맞는 식당 목록 출력하기.sql

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,13 +1,25 @@
 -- 코드를 입력하세요
+## having을 이용한 정답 출력
 select member_name, review_text, to_char(review_date,'YYYY-MM-DD') review_date
 from member_profile A, rest_review B
 where A.member_id = B.member_id
 and a.member_id in      (select member_id
-                        from rest_review
-                        group by member_id
-                        having count(*) =  (select max(count(member_id))
-                                            from rest_review
-                                            group by member_id)) 
+                         from rest_review
+                         group by member_id
+                         having count(*) =  (select max(count(member_id))
+                                             from rest_review
+                                             group by member_id)) 
 order by review_date, review_text
 
-                        
+----
+## Rank() 함수를 이용한 정답 출력
+select member_name, review_text, to_char(review_date,'YYYY-MM-DD') review_date
+from member_profile A, rest_review B
+where A.member_id = B.member_id
+and a.member_id in (select member_id
+                    from (
+                        select member_id , rank() over(order by count(*) desc) rnk
+                        from rest_review
+                        group by member_id) 
+                    where rnk =1 )
+order by review_date, review_text
