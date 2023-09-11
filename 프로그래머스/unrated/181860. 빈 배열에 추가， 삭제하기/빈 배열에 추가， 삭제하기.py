9aa71c1a91dec6a70a9f0cc55339d362d7798c2c
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-09-11 23:57:08 +0900

    Update 빈 배열에 추가， 삭제하기.py
    다른 문제풀이 방식 추가

--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -1,10 +1,16 @@
 def solution(arr, flag):
+    # answer = []
+    # for j,i in zip(arr,flag):
+    #     if i:
+    #         [answer.append(j) for u in list(range(j*2))]
+    #     else:
+    #         [answer.pop() for k in list(range(j))]
+    # return answer
+
     answer = []
     for j,i in zip(arr,flag):
         if i:
-            [answer.append(j) for u in list(range(j*2))]
+            answer += [j]*j*2
         else:
-            [answer.pop() for k in list(range(j))]
-            
-            
+            answer = answer[:-j]
     return answer
