4667e7ea48c9cbf5358df2a406e56f1cbaaa63be
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-09-12 23:58:26 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,10 +1,10 @@
 def solution(my_string, s, e):
-    if s==e:
-        return my_string
-    elif s==0 and len(my_string) == e+1:
-        return my_string[::-1]
-    elif s==0 :
-        return my_string[e-1::-1] + my_string[e:]
-    else:
-        return my_string[:s] + my_string[e:s-1:-1] + my_string[e+1:]
-    
+    # if s==e:
+    #     return my_string
+    # elif s==0 and len(my_string) == e+1:
+    #     return my_string[::-1]
+    # elif s==0 :
+    #     return my_string[e-1::-1] + my_string[e:]
+    # else:
+    #     return my_string[:s] + my_string[e:s-1:-1] + my_string[e+1:]
+    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
