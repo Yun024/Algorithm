44d150f66435cac2e89e171bfbd04d3e446a0102
Yeojun <52143231+Yun024@users.noreply.github.com>
2023-09-13 00:33:37 +0900

    [unrated] Title: 세 개의 구분자, Time: 6.87 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/README.md
+++ 프로그래머스/unrated/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [unrated] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862#) 
 
 ### 성능 요약
 
-메모리: 20.5 MB, 시간: 77.51 ms
+메모리: 21.3 MB, 시간: 6.87 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,13 +1,18 @@
 def solution(myStr):
-    word = ""
-    answer = []
-    key = ["a","b","c"]
-    for i in myStr:
-        if i not in key:
-            word += i
-        else:
-            if len(word) !=0 :  answer.append(word)
-            word = ""
-    answer.append(word)
-    return answer if answer != [""] else ["EMPTY"]
+    # word = ""
+    # answer = []
+    # key = ["a","b","c"]
+    # for i in myStr:
+    #     if i not in key:
+    #         word += i
+    #     else:
+    #         if len(word) !=0 :  answer.append(word)
+    #         word = ""
+    # answer.append(word)
+    # return answer if answer != [""] else ["EMPTY"]
+    answer = myStr.replace("a"," ").replace("b"," ").replace("c"," ").split()
+    return answer if answer else ["EMPTY"]
+    
+
+
     
