e730aced538ef6c7276c93466e44ec0bf9c29c10
Yeojun <52143231+Yun024@users.noreply.github.com>
2024-09-24 01:02:39 +0900

    Update 언어별 개발자 분류하기.sql
    group_concat미사용 및 사용 알고리즘 각각 추가

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,20 +1,32 @@
-select * from (
+### Group_concat 사용 
+# select 
+# case 
+#     when group_concat(name) regexp 'Python' and group_concat(category) regexp 'Front End' then 'A'
+#     when group_concat(name) regexp 'C#' then 'B'
+#     when group_concat(category) regexp 'Front End' then 'C'
+#     else null
+# end as grade,
+# id,
+# email
+# from developers a, skillcodes b
+# where a.skill_code & b.code != 0 
+# group by email, id
+# having grade is not null
+# order by grade, id
+
+### group_concat 미사용 및 이중 when 사용 
 select 
 case 
-    when sk_nm regexp 'Python' and sk_cat regexp 'Front End' then 'A'
-    when sk_nm regexp 'C#' then 'B'
-    when sk_cat regexp 'Front End' then 'C'
+    when sum(case when b.name = 'Python' then 10000 
+             when b.category ='Front End' then 1 else 0 end) > 10000 then 'A'
+    when sum(case when b.name ='C#' then 1 else 0 end ) > 0 then 'B'
+    when sum(case when b.category = 'Front End' then 1 else 0 end) > 0 then 'C'
     else null
 end as grade,
-id, email
-from (
-    select 
-    group_concat(name separator ',') as sk_nm,
-    group_concat(category separator ',') as sk_cat,
-    id, email
-    from developers a, skillcodes b
-    where a.skill_code & b.code != 0 
-    group by id, email
-) a) b
-where grade is not null
-order by grade, id asc
+id,
+email
+from developers a, skillcodes b
+where a.skill_code & b.code != 0 
+group by id, email
+having grade is not null
+order by grade, id 
