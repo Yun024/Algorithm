d2c6bb6b41554a28095a7b780767f296c2bcd46f
Yeojun <52143231+Yun024@users.noreply.github.com>
2024-09-26 02:57:00 +0900

    재확인

--- 온라인 강의/데이터 과학을 위한 파이썬 NumPy Basic/01. NumPy 시작/파이썬 실습 환경 구성.py
+++ 온라인 강의/데이터 과학을 위한 파이썬 NumPy Basic/01. NumPy 시작/파이썬 실습 환경 구성.py
@@ -1,31 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 a,b = 10,15
 print(a+b)
 
-
-# In[ ]:
-
-
 # 마크다운
-
-
-# 텍스트입니다.
+#텍스트입니다.
 # # 테스트
 # # **테스트**
 
-# In[ ]:
-
-
 #!pip install numpy
-
-
-# In[ ]:
-
 
 import numpy as np
 
