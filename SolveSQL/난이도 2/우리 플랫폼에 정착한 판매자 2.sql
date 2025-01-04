9b9098974a1fd3ebade6dc383674717bbc177a14
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-01-04 23:53:00 +0900

    [Upload]SolveSQL 일일 문제풀이

---
+++ SolveSQL/난이도 2/우리 플랫폼에 정착한 판매자 2.sql
@@ -0,0 +1,8 @@
+## Url : https://solvesql.com/problems/settled-sellers-2/
+
+select seller_id, count(distinct order_id) orders
+from olist_order_items_dataset
+where price >= 50
+group by seller_id
+having count(distinct order_id) >= 100
+order by orders desc
