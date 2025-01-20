d6f2516ffa901887389aeffcf61865d7a7cedf5b
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-01-20 17:08:41 +0900

    [Silver III] Title: 소수 구하기, Time: 220 ms, Memory: 42624 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,15 @@
-def isprime(num):
-    if num <= 1:
-        return False
-    for i in range(2,int(num**0.5)+1):
-        if num%i == 0:
-            return False
-    return True
-
 import sys
 m,n = map(int,sys.stdin.readline().split())
-primes = [i for i in range(m,n+1) if isprime(i)]
+def isprime(m,n):
+    primes = [True] * (n+1)
+    primes[0] = primes[1] = False
+    for i in range(2,n+1):
+        if primes[i]:
+            for j in range(i*i, n+1, i):
+                primes[j] = False
+                
+    return [j for j,i in enumerate(primes) if i and j>=m]
+
+primes = isprime(m,n)
 for i in primes:
     print(i)
