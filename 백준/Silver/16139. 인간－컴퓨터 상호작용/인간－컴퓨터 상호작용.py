f7746bea214ec1a1adf784c989f72f625c806ee5
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-05-05 01:30:46 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 692 ms, Memory: 150836 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,10 +1,30 @@
 import sys
 
+def prefix_sum(S,inpt):
+
+    dp = [[0] * 26 for _ in range(len(S))]   
+    for j,i in enumerate(S):
+        dp[j] = dp[j-1][:]
+        dp[j][ord(i)%97] += 1
+
+    ans = []
+    for alpha,l,r in inpt:
+        
+        num = ord(alpha)%97
+        if not int(l):
+            temp = dp[int(r)][num]
+        else:
+            temp = dp[int(r)][num] - dp[int(l)-1][num]
+        ans.append(temp)
+
+    return ans
+
 input = sys.stdin.readline
 
 S = input().strip()
 n = int(input().strip())
+inpt = [input().split() for _ in range(n)]
+ans = prefix_sum(S,inpt)
 
-for _ in range(n):
-    alpha, l, r = input().split()
-    print(len([i for i in S[int(l):int(r)+1] if i == alpha]))
+for i in ans:
+    print(i)
