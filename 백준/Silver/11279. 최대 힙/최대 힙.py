9198f96c3ca67f03a587dd2636024da88e0c45cf
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-05-30 23:52:03 +0900

    [Silver II] Title: 최대 힙, Time: 104 ms, Memory: 44892 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+import sys
+nums = list(map(lambda x: int(x), sys.stdin.read().strip().split()))[1:]
+
+import heapq
+
+heap = []
+for num in nums:
+    if num == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap,-num)
