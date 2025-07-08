b39ccc357f5067c2c004bac5a6db5d5ceb577232
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-07-08 22:02:42 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.08 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fn1) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 0.11 ms
+메모리: 9.3 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 21:33:08
+2025년 07월 08일 22:02:40
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,32 +1,39 @@
-def solution(places):
+class Distancechecker:
+
+    def __init__(self,places):
+        self.places = places
+        self.directions = [(0,1),(1,0),(-1,0),(0,-1)]
+    
+    def check_all(self):
+        return [self.is_safety(place) for place in self.places]
+    
+    def is_safety(self,place):
+        self.visited = [[0]*5 for _ in range(5)]
         
-    def is_safety(place):
-        
-        def dfs(x,y,n):
-            ans = 1
-            
-            if n == 2:
-                return 1
-            
-            for dx,dy in directions:
-                nx,ny = x+dx,y+dy
-                if 0 <= nx < 5 and 0 <= ny < 5 and not visited[nx][ny] and place[nx][ny] != "X" :
-                    if place[nx][ny] =='P':
-                        return 0
-                    visited[nx][ny] = 1
-                    ans *= dfs(nx,ny,n+1)
-                    
-            return ans
-                
-        visited = [[0]*5 for _ in range(5)]
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         for x in range(5):
             for y in range(5):
-                if not visited[x][y] and place[x][y] == 'P':
-                    visited[x][y] = 1
-                    if not dfs(x,y,0):
+                if not self.visited[x][y] and place[x][y] == 'P':
+                    self.visited[x][y] = 1
+                    if not self.dfs(place,x,y,0):
                         return 0
         return 1
     
-    return [is_safety(place) for place in places]
+    def dfs(self,place,x,y,depth):
+        if depth == 2:
+            return 1
         
+        for dx,dy in self.directions:
+            nx,ny = x+dx,y+dy
+            if 0 <= nx < 5 and 0 <= ny < 5 and not self.visited[nx][ny] and place[nx][ny] != "X":
+                if place[nx][ny] == "P":
+                    return 0
+                self.visited[nx][ny] = 1
+                if not self.dfs(place,nx,ny,depth+1):
+                    return 0
+                
+        return 1
+
+def solution(places):
+    
+    checker = Distancechecker(places)
+    return checker.check_all()
