5e45d912dd272f2393b8db3455362284da0ca35d
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-07-29 23:40:38 +0900

    [Silver V] Title: 야구 시즌, Time: 40 ms, Memory: 34536 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21760. 야구 시즌/야구 시즌.py
+++ 백준/Silver/21760. 야구 시즌/야구 시즌.py
@@ -6,7 +6,7 @@ for _ in range(t):
     
     n,m,k,d = map(int,input().split())
     A = n*math.comb(m,2)*k
-    B = m**n
+    B = math.comb(n,2)*m**2
     temp = A+B
     ans = temp * (d//temp)
     if not ans:
