0018d03d8bdef8af2f69a4fb333e1364825f2c89
Yeojun <52143231+Yun024@users.noreply.github.com>
2025-12-17 18:47:10 +0900

    [Bronze III] Title: 심준의 병역판정검사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25285. 심준의 병역판정검사/심준의 병역판정검사.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+for _ in range(n):
+    height,weight = map(int,input().split())
+    bmi = weight / (height ** 2 / 10000)
+    if height < 141:
+        print(6)
+    elif height < 146:
+        print(5)
+    elif height < 159:
+        print(4)
+    elif height < 161:
+        if 16 <= bmi < 35:
+            print(3)
+        else:
+            print(4)
+    else:
+        if 20 <= bmi < 25:
+            print(1)
+        elif 18.5 <= bmi < 20 or 25 <= bmi < 30:
+            print(2)
+        elif 16 <= bmi < 18.5 or 30 <= bmi < 35:
+            print(3)
+        else:
+            print(4)
